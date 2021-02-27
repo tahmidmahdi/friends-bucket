66a603b1964d2da0dcd1942a35c9ddc1e18ae00b
tahmidmahdi <tahmid5550@gmail.com>
2021-02-28 00:43:44 +0600

    header and home is designed

---
+++ src/component/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div className='home'>
+            <div className='user-container'>
+                <h1>This will contain user info</h1>
+            </div>
+            <div className ='cart-container'>
+                <h1>This will contain cart</h1>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Home;
